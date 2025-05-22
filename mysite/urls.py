c1dc5d5906cf77938a0123701c48abc97aa2c1b3
Andrew <koroyed3883@gmail.com>
2025-05-22 11:34:22 +0000

    start implementing generic views

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,22 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-from django.urls import include
-from django.views.generic import RedirectView
-
 urlpatterns += [
     path('blog/', include('blog.urls')),                           # include() adds paths from blog application
     path('', RedirectView.as_view(url='blog/', permanent=True)),   # URL map that redirects the base URL to blog application
 ]
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)      # using static() to add URL mapping to serve static files during dev
