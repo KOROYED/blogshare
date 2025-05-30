7984eefc3f1a8e621007707b194d57af893fec4e
Andrew <koroyed3883@gmail.com>
2025-05-30 17:03:38 +0000

    add pagination and make category view generic

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Post, Comment
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment, Category
 from django.views import generic
 
 def blog_index(request):
@@ -9,13 +10,31 @@ def blog_index(request):
     }
     return render(request, "blog/index.html", context=context)
 
-def blog_category(request, category):
-    posts = Post.objects.filter(category__name__contains = category).order_by("-created_on")        # category__name coz many to many field
-    context = {
-        "category": category,
-        "posts": posts,
-    }
-    return render(request, "blog/category.html", context=context)
+
+class PostByCategoryListView(generic.ListView):
+    model = Post
+    template_name = 'blog/category.html'
+    
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, name=self.kwargs["category"])
+        return Post.objects.filter(category=self.category)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.category
+        return context
+
+class PostListView(generic.ListView):
+    model = Post
+    context_object_name = "post_list"
+    paginate_by = 2
+    ordering = ["-created_on"]
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
 
 # def blog_detail(request, pk):
 #     post = Post.objects.get(pk=pk)                                                                  # pk = primary key
@@ -27,13 +46,10 @@ def blog_category(request, category):
     
 #     return render(request, "blog/detail.html", context)
 
-
-class PostListView(generic.ListView):
-    model = Post
-    context_object_name = "post_list"
-    #queryset = Post.objects.filter(title__icontains='poe')[:5]                                      # Get 5 posts containing title 'poe'
-    template_name = 'posts/post_list.html'                                                          # Specify your own template name/location
-
-
-class PostDetailView(generic.DetailView):
-    model = Post
+# def blog_category(request, category):
+#     posts = Post.objects.filter(category__name__contains = category).order_by("-created_on")        # category__name coz many to many field
+#     context = {
+#         "category": category,
+#         "posts": posts,
+#     }
+#     return render(request, "blog/category.html", context=context)
