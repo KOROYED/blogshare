947d099479c7ecb5f19394107e082c4d763b1d5d
Andrew <koroyed3883@gmail.com>
2025-06-03 09:47:00 +0000

    create author list page

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("category/<category>/", views.PostByCategoryListView.as_view(), name="blog_category"),
     path("posts/", views.PostListView.as_view(), name="blog_posts"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="blog_detail"),
+    path("authors/", views.AuthorListView.as_view(), name="blog_authors"),
     #re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),                    #same as above
     
 ]
