b2df66e8dc5787d3ac9fab1e709713dc515981cd
Andrew <koroyed3883@gmail.com>
2025-11-17 16:04:44 +0000

    make so that user can write comments to posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from blog.models import Comment
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
@@ -10,3 +12,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','first_name','last_name','email','password1','password2',)
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        labels = {'body': 'Comment'}
